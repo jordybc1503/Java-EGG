ad4011a4a4e7f72c944dc02800fe5051629fa921
Cesar Jordi <jordybc15@gmail.com>
2024-12-18 22:08:53 -0500

    Actividad:Fibonacci

--- 01 Intro a JAVA/15  Actividad Integradora III/Fibonacci.java
+++ 01 Intro a JAVA/15  Actividad Integradora III/Fibonacci.java
@@ -1,4 +1,24 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Fibonacci {
-    
+    public static void main(String[] args) {
+        int i = 2;
+
+
+        List<Integer> fibonacci = new ArrayList<>();
+        fibonacci.add(0);
+        fibonacci.add(1);
+        System.out.println("Position " + i  + ": " + fibonacci.get(0));
+        System.out.println("Position " + i  + ": " + fibonacci.get(1));
+
+        while (i <= 8) {
+            fibonacci.add(fibonacci.get(i-1)+fibonacci.get(i-2));
+            System.out.println("Position " + i  + ": " + fibonacci.get(i));
+            i++;
+        }
+        
+      
+    }
     
 }
